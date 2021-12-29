285377645d1d4ab70afd6f6e58b0b5fb0e09ca03
Mitchelle <creadomitchelle@gmail.com>
2021-12-29 14:54:27 +0530

    user registration & login

---
+++ migrations/1555584527837_add-users.js
@@ -0,0 +1,36 @@
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.createTable('users', {
+    id: 'id',
+    email: {
+      type: 'varchar(255)',
+      notNull: true
+    },
+    password: {
+      type: 'varchar(72)',
+      notNull: true
+    },
+    firstName: {
+      type: 'varchar(255)',
+      notNull: true
+    },
+    mobileNo: {
+      type: 'varchar(255)',
+      notNull: true
+    },
+    lastName: {
+      type: 'varchar(255)',
+      notNull: true
+    },
+    createdAt: {
+      type: 'timestamp',
+      notNull: true,
+      default: pgm.func('current_timestamp')
+    }
+  });
+};
+
+exports.down = (pgm) => {
+
+};
